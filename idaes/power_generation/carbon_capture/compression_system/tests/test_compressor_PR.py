17b5a5016e0c04b7ef0198bea51fdf5814f5638c
minhwvu <45834279+minhwvu@users.noreply.github.com>
2021-07-07 11:19:21 -0400

    new test

--- idaes/power_generation/carbon_capture/compression_system/tests/test_compressor_PR.py
+++ idaes/power_generation/carbon_capture/compression_system/tests/test_compressor_PR.py
@@ -12,7 +12,6 @@
 ##############################################################################
 """
 Pytest for Compression Model
-
 """
 import pytest
 # Import Pyomo libraries
@@ -27,13 +26,16 @@ from idaes.generic_models.properties.core.generic.generic_property import (
 from idaes.core.util.model_statistics import degrees_of_freedom
 
 # Import Unit Model Modules
-from idaes.power_generation.carbon_capture.compression_system.compressor \
+# from idaes.power_generation.carbon_capture.compression_system.compressor_PR \
+#       import (CompressionStage, VaneDiffuserType, ImpellerType)
+from compressor_PR \
       import (CompressionStage, VaneDiffuserType, ImpellerType)
 
 # Import Property Package
 from idaes.power_generation.properties.natural_gas_PR import get_prop
 
 from idaes.core.util.testing import get_default_solver, initialization_tester
+import idaes.logger as idaeslog
 
 # -----------------------------------------------------------------------------
 # Get default solver for testing
@@ -56,27 +58,62 @@ def build_unit():
                  "vane_diffuser_type": VaneDiffuserType.vane_diffuser})
 
     # Set the compressor inlet conditions and an initial flow guess
-    p = 1.12308 * 1e5  # Pa
-    t = 38.4735 + 273.15  # K
-    fin = 1698.37  # mol/s
+    p = 1.050328097156878 * 1e5  # Pa
+    t = 40.0113 + 273.15  # K
+    fin = 1579.798470673201  # mol/s
 
-    m.fs.unit.inlet.flow_mol[:].fix(fin)
+    m.fs.unit.inlet.flow_mol.fix(fin)
     m.fs.unit.inlet.temperature.fix(t)
     m.fs.unit.inlet.pressure[:].fix(p)
-    m.fs.unit.inlet.mole_frac_comp[:, "H2O"].fix(0.061396)
-    m.fs.unit.inlet.mole_frac_comp[:, "CO2"].fix(0.938604)
-
-    # inlet specifications
-    m.fs.unit.U2.fix(315.3)
-    m.fs.unit.outlet.pressure[:].fix(2.73423*1e5)
+    m.fs.unit.inlet.mole_frac_comp[:, "H2O"].fix(0.0601355)
+    m.fs.unit.inlet.mole_frac_comp[:, "CO2"].fix(0.939864)
 
     # fix compressor specification
-    m.fs.unit.speed_of_sound.fix(279.214)
+    m.fs.unit.speed_of_sound.fix(279.7815969751224)
+    m.fs.unit.heat_capacity_ratio.fix(1.2772)
+    m.fs.unit.U2.fix(315.3)
     m.fs.unit.r2.fix(0.67654)
     m.fs.unit.z_s.fix(0.9952)
     m.fs.unit.z_d1.fix(0.97373)
     m.fs.unit.efficiency_mech.fix(0.97)
     m.fs.unit.eff_drive.fix(1.0)
+
+    m.fs.unit.initialize(outlvl=idaeslog.INFO_HIGH)
+
+    m.fs.compstage1_coeff_a = pyo.Var(
+        m.fs.time, doc="coefficient a")
+    m.fs.compstage1_coeff_b = pyo.Var(
+        m.fs.time, doc="coefficient b")
+    m.fs.compstage1_coeff_c = pyo.Var(
+        m.fs.time, doc="coefficient c")
+    m.fs.compstage1_Ang = pyo.Var(m.fs.time, doc="IGV")
+
+    @m.fs.Constraint(m.fs.time)
+    def coeff_a_eqn(b, t):
+        return b.compstage1_coeff_a[t] == -38.46 * pyo.exp(
+            -0.005098 * b.compstage1_Ang[t]) + (- 0.1043) * pyo.exp(
+                0.09821 * b.compstage1_Ang[t])
+
+    @m.fs.Constraint(m.fs.time)
+    def coeff_b_eqn(b, t):
+        return b.compstage1_coeff_b[t] == 6.863 * pyo.exp(
+              -0.01329 * b.compstage1_Ang[t]) \
+            + 0.02018 * pyo.exp(0.09443 * b.compstage1_Ang[t])
+
+    @m.fs.Constraint(m.fs.time)
+    def coeff_c_eqn(b, t):
+        return b.compstage1_coeff_c[t] == -0.00891 * pyo.exp(
+            0.06757 * b.compstage1_Ang[t]) + 0.9661 * pyo.exp(
+                0.006228 * b.compstage1_Ang[t])
+
+    @m.fs.Constraint(
+        m.fs.time, doc="Correlation between psi_s and psi_3")
+    def psi_s_stage1_eqn(b, t):
+        return b.unit.psi_s[t] == b.compstage1_coeff_a[t] \
+            * b.unit.psi_3[t]**2 + b.compstage1_coeff_b[
+                t] * b.unit.psi_3[t] + b.compstage1_coeff_c[t]
+
+    m.fs.unit.ratioP.fix(2.375530641906031)
 
     return m
 
@@ -118,11 +155,16 @@ def test_run(build_unit):
     assert results.solver.status == pyo.SolverStatus.ok
     assert degrees_of_freedom(m) == 0
 
-    # work
-    assert (pytest.approx(5092763.0, abs=0.1) ==
-            pyo.value(m.fs.unit.work_mechanical[0]))
+    # energy balance
+    assert (pytest.approx(0, abs=1e-3) ==
+            pyo.value(m.fs.unit.inlet.flow_mol[0]
+                      * m.fs.unit.control_volume.properties_in[0].enth_mol
+                      - m.fs.unit.outlet.flow_mol[0]
+                      * m.fs.unit.control_volume.properties_out[0].enth_mol
+                      + m.fs.unit.work_mechanical[0]))
+
     # pressure change
-    assert (pytest.approx(161115.0, abs=0.1) ==
+    assert (pytest.approx(144475.8, abs=0.1) ==
             pyo.value(m.fs.unit.deltaP[0]))
     # mass balance
     assert (pytest.approx(0, abs=1e-2) ==
